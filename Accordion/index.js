baca36d3a90b56a64dcae49aeeb69ee9a63c44dd
FredYang201 <fredyang1402143595@gmail.com>
2019-12-12 23:40:35 -0800

    deleting

--- Accordion/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  var $lis = $('div li');
-  $lis.mouseover(function(){
-    $(this).stop(true).animate({width:500},1000).siblings().stop(true).animate({width:140},1000);
-  }).mouseout(function(){ //鼠标离开时，将所有图片所在li变为默认宽
-    $lis.stop(true).animate({width:200},1000);
-  });
-})
